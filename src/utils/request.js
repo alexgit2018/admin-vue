2c0ede98dfb6c7d1095e7febc41875d233494f63
aleximageregistry <114698176@qq.com>
2021-07-15 17:27:22 +0800

    '202101715'

---
+++ src/utils/request.js
@@ -0,0 +1,28 @@
+﻿import axios from 'axios' // 导入node_modules里的axios
+import { getToken } from '@/utils/token'
+//axios.defaults.baseURL = process.env.API_ROOT; // 后端测试接口 ip:port
+// axios.defaults.baseURL = window.api.BASE_URL;
+const service = axios.create({
+    baseURL: window.api.BASE_URL
+})
+
+service.interceptors.request.use((request) => {
+    request.headers['SignToken'] = getToken()
+    return request
+}, (error) => {
+    console.log('错误的传参');
+    return Promise.reject(error);
+});
+
+service.interceptors.response.use(response => {
+    const data = response.data
+    if(data){
+        switch(data.errno){
+            case 500:
+            default:
+        }
+    }
+    return data
+})
+
+export default service // 导出axios
---
+++ static/config.js
@@ -0,0 +1,5 @@
+window.api={
+  //BASE_URL : "https://59.52.96.148:9101",
+  BASE_URL : "http://localhost:8088",
+}
+window.projectname = '江西高速'
